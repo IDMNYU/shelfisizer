db26d412b48760c39340b9b55d00fc24c3684e93
rev3rend <rev3rend@gmail.com>
2022-01-14 10:16:49 -0500

    Update 13_KEYBOARD.ino

--- arduino/13_KEYBOARD/13_KEYBOARD.ino
+++ arduino/13_KEYBOARD/13_KEYBOARD.ino
@@ -16,7 +16,7 @@
 #include <Adafruit_SSD1306.h>
 
 #ifndef _BV
-#define _BV(bit) (1 << (bit)) 
+#define _BV(bit) (1 << (bit))
 #endif
 
 #define DEBUG false
@@ -77,11 +77,11 @@ char *notes[] = {"C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A
 char *arpmodes[] = {"UP ", "DN ", "UP/DN ", "RND "};
 
 void setup() {
-  if(DEBUG) {
+  if (DEBUG) {
     Serial.begin(9600);
     while (!Serial) { // needed to keep leonardo/micro from starting too fast!
       delay(10);
-   }
+    }
   }
 
   pinMode(2, OUTPUT); // trigger A
@@ -93,8 +93,8 @@ void setup() {
   pinMode(4, INPUT_PULLUP); // start/stop
   pinMode(9, OUTPUT); // clock
 
-  if(DEBUG) Serial.println("YAY KEYBOARD"); 
-  
+  if (DEBUG) Serial.println("YAY KEYBOARD");
+
   // Default address is 0x5A, if tied to 3.3V its 0x5B
   // If tied to SDA its 0x5C and if SCL then 0x5D
   if (!cap1.begin(0x5D)) {
@@ -128,10 +128,10 @@ void setup() {
   display.display();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.println("Hi there");
   display.println("YAY");
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.display(); // actually display all of the above
 
 
@@ -142,96 +142,103 @@ void setup() {
 }
 
 void loop() {
+
+  //
+  // INPUTS
+  //
+
+  // PADS
+  
   // Get the currently touched pads
   currtouched1 = cap1.touched(); // bitmask
   currtouched2 = cap2.touched(); // bitmask
   currtouched3 = cap3.touched(); // bitmask
   currtouched4 = cap4.touched(); // bitmask
 
-  for (uint8_t i=0; i<12; i++) {
+  for (uint8_t i = 0; i < 12; i++) {
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched1 & _BV(i)) && !(lasttouched1 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keydown A "); Serial.println(i);
-      if(i==4) latch = !latch; // latch key
-      if(i==3) arpmode = (arpmode+1)%4; // arpeggio mode
-      if(latch==false) // clear arp queue
+      if (DEBUG) Serial.print("keydown A "); Serial.println(i);
+      if (i == 4) latch = !latch; // latch key
+      if (i == 3) arpmode = (arpmode + 1) % 4; // arpeggio mode
+      if (latch == false) // clear arp queue
       {
-        pcount=0;
-        for(int i = 0;i<37;i++)
+        pcount = 0;
+        for (int i = 0; i < 37; i++)
         {
           arp[i] = false;
         }
       }
-      if(i>4) // notes only
+      if (i > 4) // notes only
       {
         pcount++; // polyphony count up
         tlen1 = TDUR; // new trigger
-        curkey = i-5;
-        arp[curkey] = latch?!arp[curkey]:true;
+        curkey = i - 5;
+        arp[curkey] = latch ? !arp[curkey] : true;
       }
     }
     // if it *was* touched and now *isnt*, alert!
     if (!(currtouched1 & _BV(i)) && (lasttouched1 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keyup A "); Serial.println(i);
-      if(i>4&&!latch) // notes only
+      if (DEBUG) Serial.print("keyup A "); Serial.println(i);
+      if (i > 4 && !latch) // notes only
       {
         pcount--; // polyphony count down
-        arp[i-5] = false;
+        arp[i - 5] = false;
       }
     }
-    
+
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched2 & _BV(i)) && !(lasttouched2 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keydown B "); Serial.println(i);
+      if (DEBUG) Serial.print("keydown B "); Serial.println(i);
       pcount++; // polyphony count up
       tlen1 = TDUR; // new trigger
-      curkey = i-5+12;
-      arp[curkey] = latch?!arp[curkey]:true;
+      curkey = i - 5 + 12;
+      arp[curkey] = latch ? !arp[curkey] : true;
     }
     // if it *was* touched and now *isnt*, alert!
     if (!(currtouched2 & _BV(i)) && (lasttouched2 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keyup B "); Serial.println(i);
-      if(!latch)
+      if (DEBUG) Serial.print("keyup B "); Serial.println(i);
+      if (!latch)
       {
         pcount--; // polyphony count down
-        arp[i-5+12] = false;
+        arp[i - 5 + 12] = false;
       }
     }
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched3 & _BV(i)) && !(lasttouched3 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keydown C "); Serial.println(i);
+      if (DEBUG) Serial.print("keydown C "); Serial.println(i);
       pcount++; // polyphony count up
       tlen1 = TDUR; // new trigger
-      curkey = i-5+24;
-      arp[curkey] = latch?!arp[curkey]:true;
+      curkey = i - 5 + 24;
+      arp[curkey] = latch ? !arp[curkey] : true;
     }
     // if it *was* touched and now *isnt*, alert!
     if (!(currtouched3 & _BV(i)) && (lasttouched3 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keyup C "); Serial.println(i);
-      if(!latch)
+      if (DEBUG) Serial.print("keyup C "); Serial.println(i);
+      if (!latch)
       {
         pcount--; // polyphony count down
-        arp[i-5+24] = false;    
+        arp[i - 5 + 24] = false;
       }
     }
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched4 & _BV(i)) && !(lasttouched4 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keydown D "); Serial.println(i);
-      if(i<6) // notes only
+      if (DEBUG) Serial.print("keydown D "); Serial.println(i);
+      if (i < 6) // notes only
       {
         pcount++; // polyphony count up
         tlen1 = TDUR; // new trigger
-        curkey = i-5+36;
-        arp[curkey] = latch?!arp[curkey]:true;
+        curkey = i - 5 + 36;
+        arp[curkey] = latch ? !arp[curkey] : true;
       }
     }
     // if it *was* touched and now *isnt*, alert!
     if (!(currtouched4 & _BV(i)) && (lasttouched4 & _BV(i)) ) {
-      if(DEBUG) Serial.print("keyup D "); Serial.println(i);
-      if(i<6&&!latch) // notes only
+      if (DEBUG) Serial.print("keyup D "); Serial.println(i);
+      if (i < 6 && !latch) // notes only
       {
         pcount--; // polyphony count down
-        arp[i-5+36] = false;
+        arp[i - 5 + 36] = false;
       }
     }
   }
@@ -242,39 +249,55 @@ void loop() {
   lasttouched3 = currtouched3;
   lasttouched4 = currtouched4;
 
-
+  // SEQUENCER CONTROLS
   
+  // start/stop and rate controls
+  bool ss = !digitalRead(4);
+  if (ss == true && oldss == false)
+  {
+    gogogo = !gogogo;
+    //    if(DEBUG) Serial.println(gogogo);
+  }
+  oldss = ss;
+
+  int r = analogRead(A0);
+  r = map(r, 0, 1023, 25, 2000);
+  rate = psmooth * rate + (1.0 - psmooth) * r;
+  //if(DEBUG) Serial.println(rate);
+
+  //
   // OUTPUTS
-
-  // LIVE KEYBOARD (UPPER OUTS)
+  //
   
+  // LIVE KEYBOARD (UPPER OUTS)
+
   // triggers and gates (2, 3, 7, 8)
-  if(tlen1>0) digitalWrite(2, HIGH); else digitalWrite(2, LOW);
-  if(pcount>0) digitalWrite(3, HIGH); else digitalWrite(3, LOW);
+  if (tlen1 > 0) digitalWrite(2, HIGH); else digitalWrite(2, LOW);
+  if (pcount > 0) digitalWrite(3, HIGH); else digitalWrite(3, LOW);
 
   // cv pitch out
-  vout1 = round((float)curkey*VPST);
+  vout1 = round((float)curkey * VPST);
   dac1.setVoltage(vout1, false);
 
   // pressure value (RC'ed through an 8k2 and a .22uF)
-  if(curkey<7) {
-    ptemp = map(cap1.filteredData(curkey+5)-POFF, 0, cap1.baselineData(curkey+5), 255, 0);
+  if (curkey < 7) {
+    ptemp = map(cap1.filteredData(curkey + 5) - POFF, 0, cap1.baselineData(curkey + 5), 255, 0);
   }
-  else if(curkey>=7&&curkey<19) {
-    ptemp = map(cap2.filteredData(curkey+5-12)-POFF, 0, cap2.baselineData(curkey+5-12), 255, 0);
+  else if (curkey >= 7 && curkey < 19) {
+    ptemp = map(cap2.filteredData(curkey + 5 - 12) - POFF, 0, cap2.baselineData(curkey + 5 - 12), 255, 0);
   }
-  else if(curkey>=19&&curkey<31) {
-    ptemp = map(cap3.filteredData(curkey+5-24)-POFF, 0, cap3.baselineData(curkey+5-24), 255, 0);
+  else if (curkey >= 19 && curkey < 31) {
+    ptemp = map(cap3.filteredData(curkey + 5 - 24) - POFF, 0, cap3.baselineData(curkey + 5 - 24), 255, 0);
   }
-  else if(curkey<37) {
-    ptemp = map(cap4.filteredData(curkey+5-36)-POFF, 0, cap4.baselineData(curkey+5-36), 255, 0);
+  else if (curkey < 37) {
+    ptemp = map(cap4.filteredData(curkey + 5 - 36) - POFF, 0, cap4.baselineData(curkey + 5 - 36), 255, 0);
   }
   ptemp = constrain(ptemp, 0, 255);
-  pressureval = psmooth*pressureval + (1.0-psmooth)*ptemp;
-//  if(DEBUG) {
-//        Serial.print("press ");
-//        Serial.println(pressureval);
-//  }
+  pressureval = psmooth * pressureval + (1.0 - psmooth) * ptemp;
+  //  if(DEBUG) {
+  //        Serial.print("press ");
+  //        Serial.println(pressureval);
+  //  }
 
   // CV pressure (5, 6)
   analogWrite(5, (int)pressureval);
@@ -282,143 +305,129 @@ void loop() {
 
   // SEQUENCER (LOWER OUTS)
 
-  // start/stop and rate controls
-  bool ss = !digitalRead(4);
-  if(ss==true&&oldss==false)
-  {
-    gogogo = !gogogo;
-//    if(DEBUG) Serial.println(gogogo);
-  }
-  oldss = ss;
-
-  int r = analogRead(A0);
-  r = map(r,0,1023,25,2000);
-  rate = psmooth*rate + (1.0-psmooth)*r;
-  //if(DEBUG) Serial.println(rate);
-
-  if(gogogo) {
+  if (gogogo) {
     now = millis();
-    if(now-then>rate) 
-    { 
+    if (now - then > rate)
+    {
       // arp sum
-      arpcount=0;
-      for(int i = 0;i<37;i++)
+      arpcount = 0;
+      for (int i = 0; i < 37; i++)
       {
-        if(arp[i]) arpcount++;
+        if (arp[i]) arpcount++;
       }
       pulseout = TDUR;
       then = now;
-      if(arpcount>0) { // arpegiate
+      if (arpcount > 0) { // arpegiate
         tlen2 = TDUR;
-        
-        if(arpmode==0) // ARP UP
+
+        if (arpmode == 0) // ARP UP
         {
-          for(int i = 0;i<37;i++)
+          for (int i = 0; i < 37; i++)
           {
-            int nn = (i+arpptr+1)%37; // next note?
-            if(arp[nn]) { // found the next note
-              arpptr = nn;
-              break;
-            }
-          }
-        }
-  
-        if(arpmode==1) // ARP DOWN
-        {
-          for(int i = 36;i>=0;i--)
-          {
-            int nn = (i+arpptr-1+37)%37; // next note?
-            if(arp[nn]) { // found the next note
+            int nn = (i + arpptr + 1) % 37; // next note?
+            if (arp[nn]) { // found the next note
               arpptr = nn;
               break;
             }
           }
         }
 
-        if(arpmode==2) // ARP UP/DOWN
+        if (arpmode == 1) // ARP DOWN
         {
-          if(!arpdir) // up
+          for (int i = 36; i >= 0; i--)
           {
-            for(int i = 0;i<37;i++)
-            {
-              int nn = (i+arpptr+1)%37; // next note?
-              if(arp[nn]) { // found the next note
-                arpptr = nn;
-                break;
-              }
-              if(i==36) arpdir = true;
-            }
-          }
-          else { // down
-            for(int i = 36;i>=0;i--)
-            {
-              int nn = (i+arpptr-1+37)%37; // next note?
-              if(arp[nn]) { // found the next note
-                arpptr = nn;
-                break;
-              }
-              if(i==0) arpdir = false;
-            }
-          }
-        }
-      
-        if(arpmode==3) // ARP RANDOM
-        {
-          int spoint = random(37);
-          for(int i = 0;i<37;i++)
-          {
-            int nn = (i+arpptr+spoint)%37; // next note?
-            if(arp[nn]) { // found the next note
+            int nn = (i + arpptr - 1 + 37) % 37; // next note?
+            if (arp[nn]) { // found the next note
               arpptr = nn;
               break;
             }
           }
         }
-      
+
+        if (arpmode == 2) // ARP UP/DOWN
+        {
+          if (!arpdir) // up
+          {
+            for (int i = 0; i < 37; i++)
+            {
+              int nn = (i + arpptr + 1) % 37; // next note?
+              if (arp[nn]) { // found the next note
+                arpptr = nn;
+                break;
+              }
+              if (nn == 36) arpdir = true;
+            }
+          }
+          else { // down
+            for (int i = 36; i >= 0; i--)
+            {
+              int nn = (i + arpptr - 1 + 37) % 37; // next note?
+              if (arp[nn]) { // found the next note
+                arpptr = nn;
+                break;
+              }
+              if (nn == 0) arpdir = false;
+            }
+          }
+        }
+
+        if (arpmode == 3) // ARP RANDOM
+        {
+          int spoint = random(37);
+          for (int i = 0; i < 37; i++)
+          {
+            int nn = (i + arpptr + spoint) % 37; // next note?
+            if (arp[nn]) { // found the next note
+              arpptr = nn;
+              break;
+            }
+          }
+        }
+
       }
     }
   }
 
   // triggers and gates (2, 3, 7, 8)
-  if(tlen2>0) digitalWrite(7, HIGH); else digitalWrite(7, LOW);
-  if(gogogo&&arpcount>0) digitalWrite(8, HIGH); else digitalWrite(8, LOW);
+  if (tlen2 > 0) digitalWrite(7, HIGH); else digitalWrite(7, LOW);
+  if (gogogo && arpcount > 0) digitalWrite(8, HIGH); else digitalWrite(8, LOW);
 
   // cv pitch out
-  vout2 = round(arpptr*VPST);
+  vout2 = round(arpptr * VPST);
   dac2.setVoltage(vout2, false);
 
-  if(gogogo) digitalWrite(9, pulseout>0);
+  if (gogogo) digitalWrite(9, pulseout > 0);
 
 
-  
-  // display 
+
+  // display
   display.clearDisplay();
 
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.print("KEY: ");
-  display.print(notes[curkey%12]);
-  display.print(curkey/12);
+  display.print(notes[curkey % 12]);
+  display.print(curkey / 12);
   display.println();
 
   display.print("ARP: ");
-  display.print(gogogo?arpmodes[arpmode]:"OFF ");
-  if(gogogo) display.print(notes[arpptr%12]);
-  if(gogogo) display.print(arpptr/12);
+  display.print(gogogo ? arpmodes[arpmode] : "OFF ");
+  if (gogogo) display.print(notes[arpptr % 12]);
+  if (gogogo) display.print(arpptr / 12);
   display.println();
 
   display.print("rate: ");
   display.print(round(rate));
 
-  display.setCursor(90,0);
-  display.print(latch?"LATCH":"");
+  display.setCursor(90, 0);
+  display.print(latch ? "LATCH" : "");
 
 
   display.display(); // actually display all of the above
- 
+
   tlen1--;
-  if(tlen1<0) tlen1=0;
+  if (tlen1 < 0) tlen1 = 0;
   tlen2--;
-  if(tlen2<0) tlen2=0;
+  if (tlen2 < 0) tlen2 = 0;
   pulseout--;
-  if(pulseout<0) pulseout=0;
+  if (pulseout < 0) pulseout = 0;
 }
